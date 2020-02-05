30585e9885dd1abf691ca5b23e97e7efab47da93
Ulisses <ulissesmercado2012@icloud.com>
2020-02-04 22:15:14 -0600

    Changing Theme data

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,68 @@
 import 'package:flutter/material.dart';
+import '../pages/direcciones_page.dart';
+import '../pages/mapas_page.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+class HomePage extends StatefulWidget {
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int curreentIndex = 0;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Hola HomePage'),),
+      appBar: AppBar(
+        title: Text('QR SScanner'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.delete_forever),
+            onPressed: (){ },
+          )
+        ],
+      ),
+      body: _callPage(curreentIndex),
+      bottomNavigationBar: _crearBottonNovigationBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.filter_center_focus),
+        onPressed: (){
+
+        },
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
     );
+  }
+
+  Widget _crearBottonNovigationBar() {
+    return BottomNavigationBar(
+      currentIndex: curreentIndex,
+      onTap: (index){
+        setState(() {
+          curreentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Mapas')
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_5),
+          title: Text('Direcciones')
+        ),
+      ],
+    );
+  }
+
+  Widget _callPage(int paginaActual) {
+    switch(paginaActual){
+      case 0: return MapasPage();
+      case 1: return DireccionesPage();
+      default: return MapasPage();
+    }
   }
 }
